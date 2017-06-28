7475f132b4734ea5e40073bf7f92f008645c2e4d
T. Shaffner <shaffnert03@yahoo.com>
2017-06-28 16:08:13 +0200

    Before deleting old approach

--- StreetLights/StreetLights.cpp
+++ StreetLights/StreetLights.cpp
@@ -37,7 +37,7 @@ struct FirstColumnOnlyCmpDouble {
     }
 };
 
-
+//! Begin Main Function (Note: Bold/Green comments act as section headings)
 int main() {
     std::ios_base::sync_with_stdio(false);
 
@@ -53,84 +53,128 @@ int main() {
         int n = params[1];
         int d = params[2];
 
-        vector<bool> lightable(l); //Initialize a lightable vector to track possibility
+        //! Get inputs
+        vector<int> light_locations = readlineofints(n);
 
-        vector<int> light_locations;
-        //Get inputs
-        light_locations = readlineofints(n);
-        //For each light
-        for (auto light : light_locations) {
-            //Calculate cone range
-            int start = max(0, light - d);
-            int end = min(l, light + d);
-            //Iterate over cone range turning on lightable flags
-            for (int k = start; k < end; k++) {
-                lightable[k] = true;
-            }
+        //Initialize indices
+        int light_index = 0;
+        int lights_on = 0;
+        bool possible = true;
+
+        //Sort lights
+        sort(light_locations.begin(), light_locations.end());
+
+
+        //Check to see if start can be illuminated
+        if (light_locations[0] - d > 0) {
+            possible = false;
         }
 
-        bool possible = true;
-        //Iterate over all the possible spaces
-        for (auto space : lightable) {
-            //If any space is not lightable, mark the whole problem as impossible and break
-            if (space == false) { 
+        //! Go through street
+        //Everything behind street_location is lit.
+        for (int street_location = 0; street_location <= l; street_location = light_locations[light_index] + d) { //Set the street location to the next dark spot)           
+
+            //If we know it's impossible, break
+            if (!possible) { break; }
+
+            //Set k to the current light
+            int k = light_index;
+            //Increment k until the light would no longer illuminate the current street location
+            while (k<light_locations.size() && light_locations[k] - d <= street_location) {
+                k++;
+            }
+
+            //Update indices
+            light_index = k - 1; //Set the light to the last one that could illuminate the current street location
+            lights_on++; //Increment lights on (since we're turning on the k-1 light)
+
+            //If this was the last light 
+            if (k == light_locations.size()) {
+                //If it lights to the end, break
+                if (light_locations[light_index] + d < l) {
+                    break;
+                } //If not, mark false and break
+                else {
+                    possible = false;
+                    break;
+                }
+            }
+            //If the gap between this light and the next is nonzero, mark false and break
+            if (light_locations[k] - d > light_locations[light_index] + d) {
                 possible = false;
                 break;
             }
         }
 
-        //TODO: Convert things below this line
-        ////Calculate the value/length ratio, tracking item number
-        //vector<vector<double>> ratio;
-        //vector<vector<int>> sorted_score;
-        //for (int i = 0; i < length.size(); i++) {
-        //    vector<double> ratio_row;
-        //    vector<int> sorted_score_row;
-        //    ratio_row.push_back(i); //Add item number
-        //    sorted_score_row.push_back(i); //Add item number
-        //    ratio_row.push_back(double(score[i]) / double(length[i])); //Add score
-        //    sorted_score_row.push_back(score[i]); //Add score
-        //    ratio.push_back(ratio_row);
-        //    sorted_score.push_back(sorted_score_row);
-        //}
 
-        ////Sort by descending order of the score and ratio
-        //sort(sorted_score.begin(), sorted_score.end(), FirstColumnOnlyCmp());
-        //sort(ratio.begin(), ratio.end(), FirstColumnOnlyCmpDouble());
 
-        //int remaining_length = m; //Initialize remaining length
-        //vector<int> items_added;
-        //while (remaining_length > 0) {
-        //    int item_to_add = -1;
-        //    for (auto item : ratio) { //Iterate through the ratio list
-        //        if (length[item[0]] <= remaining_length) { //If the length of the next best item is small enough to fit, greedy solution found
-        //            item_to_add = item[0]; //Mark item as one to add
-        //            break; //Break out of loop
-        //        }
-        //    }
-        //    if (item_to_add == -1 && length[sorted_score[0][0]]<remaining_length) { //If the most valuable item is small enough and better value
-        //        item_to_add = sorted_score[0][0]; //Use it instead
-        //    }
-        //    if (item_to_add != -1) { //If there's still an eligible item to add
-        //        items_added.push_back(item_to_add); //Record item added
-        //        remaining_length -= length[item_to_add]; //Decrement length
-        //    }
-        //    else { //Otherwise
-        //        break; //Give up
-        //    }
-        //}
 
-        //Print output
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //! Go through street
+        //Everything behind street_location is lit.
+        for (int street_location = 0; street_location <= l; street_location = light_locations[light_index] + d){ //Set the street location to the next dark spot)            
+
+            //If we're past the last light and there are still unlit sections at the end, mark false and break
+            if (light_locations.size() >= light_index + 1 && light_locations[light_index] + d < street_location) {
+                possible = false;
+            }
+            //If we're past the last light but this section is the end
+            else if (light_locations.size() >= light_index + 1 && street_location == l) {
+                street_location++; //Increment street location to leave loop
+            }
+            //If the next light is unable to illuminate this spot and we're not at the start mark the problem impossible and break
+            else if (light_locations[light_index + 1] - d > street_location && street_location != 0) {
+                possible = false;
+            }
+            
+            //If we know it's impossible, break
+            if (!possible) { break; }
+            
+            //Set k to the current light
+            int k = light_index;
+            //Increment k until the light would no longer illuminate the current street location
+            while (k<light_locations.size() && light_locations[k] - d <= street_location) {
+                k++;
+            }
+            
+            //Update indices
+            light_index = k-1; //Set the light to the last one that could illuminate the current street location
+            lights_on++; //Increment lights on (since we're turning on the k-1 light)
+        }
+
+
+
+
+        //! Print output
         std::cout << "Case #" << i << ": ";
         if (possible) {
-
+            std::cout << lights_on;
         }
         else {
             std::cout << "impossible";
         }
-        /*for (auto item : items_added) {
-            std::cout << item + 1 << " ";
-        }*/
         std::cout << std::endl;
     }
     return 0;
